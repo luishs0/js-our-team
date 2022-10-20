02f1ffcf209b519ee5c479da7ea83493e4627c28
luishs0 <luishs.doc@gmail.com>
2022-10-20 14:44:15 +0200

    MILESTONE 0

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let teamArray = [
+    {
+        name: "Wayne Barnett",
+        roll: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        roll: "Chief Editor",
+        img: "wangela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        roll: "Office Manager",
+        img: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        roll: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        roll: "Developer",
+        img: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        roll: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg"
+    }
+
+];
